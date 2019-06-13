bc6cad89591c52db352c14b44232ff22701be0c0
lnsantos <39661894+lnsantos@users.noreply.github.com>
2019-06-13 15:22:13 -0300

    Atendente Relatório Back-end

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const path = require('path');
+var fs = require('fs');
 
 const app = express();
 var server = require('http').createServer(app);
@@ -30,6 +31,14 @@ io.on('connection', socket =>{
     
     console.log(`Protocol WebSocket open : ${socket.id}`);
 
+    socket.on('relatorioAtendido', data=>{
+        console.log(data);
+        fs.appendFile(`atividades/${data.codigoProceso}.txt`, `\n -- RELATÓRIO :  ${data.string} \n`, function (err) {
+            if (err) throw err;
+            console.log(`${data.codigoProceso} Atualizado com sucesso! ( Relatório atualizado )`);
+          });
+    });
+
     socket.on('sendRedirect', dataCode =>{
         orderByClient.push(dataCode);
         console.log('Enviado para Client-side : ' + dataCode.lastNumber);
@@ -44,9 +53,16 @@ io.on('connection', socket =>{
     socket.on('sendUser', dataUser =>{
         orderBySelect.push(dataUser);
         console.log('Enviado para Client-side Atendente: ' + dataUser.userSelect);
+        fs.appendFile(`atividades/${dataUser.userSelect}.txt`,'\n CRIADO EM : '+new Date()+
+        ' POR Guinchê : '+dataUser.where +" \n",(err)=>{
+            if(err) throw err;
+            else console.log('Arquivo '+dataUser.userSelect+' criado com sucesso!')
+        });
         socket.broadcast.emit('receivedUser', dataUser);
     });
 
 });
+
+
 
 server.listen(3000);
